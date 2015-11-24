6f4d58292d531025f2692c79f09f341f3a6de0a9
hugobarzano <hugobarzano@gmail.com>
2015-11-24 13:15:33 +0100

    pruebas openshift 7

--- ProyectoDjango/wsgi.py
+++ ProyectoDjango/wsgi.py
@@ -18,20 +18,9 @@ from dj_static import Cling
 ON_OPENSHIFT = os.environ.get('OPENSHIFT_POSTGRESQL_PORT')
 if ON_OPENSHIFT:
 	os.environ['OPENSHIFT_PYTHON_SERVER'] = "gunicorn"
-	virtenv = os.environ['APPDIR'] + '/virtenv/'
-	os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.6/site-packages')
-	virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-	try:
-    		execfile(virtualenv, dict(__file__=virtualenv))
-	except:
-    		pass
 
-	os.environ['DJANGO_SETTINGS_MODULE'] = os.environ['OPENSHIFT_APP_NAME']+'.settings'
-	sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', os.environ['OPENSHIFT_APP_NAME']))
-	application = django.core.handlers.wsgi.WSGIHandler()
 
-else:
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProyectoDjango.settings")
-	application = get_wsgi_application()
-	application = Cling(get_wsgi_application())
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProyectoDjango.settings")
+application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 
