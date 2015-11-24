731c4f3ee040b339c9a8f7a798691d72e4c49557
hugobarzano <hugobarzano@gmail.com>
2015-11-24 11:55:04 +0100

    deploy en opehshift

--- ProyectoDjango/wsgi.py
+++ ProyectoDjango/wsgi.py
@@ -8,13 +8,28 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+import django.core.handlers.wsgi
 from django.core.wsgi import get_wsgi_application
 
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProyectoDjango.settings")
+ON_OPENSHIFT = os.environ.get('OPENSHIFT_POSTGRESQL_PORT')
+if ON_OPENSHIFT:
+	virtenv = os.environ['APPDIR'] + '/virtenv/'
+	os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.6/site-packages')
+	virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+	try:
+    		execfile(virtualenv, dict(__file__=virtualenv))
+	except:
+    		pass
 
-application = get_wsgi_application()
-application = Cling(get_wsgi_application())
+	os.environ['DJANGO_SETTINGS_MODULE'] = os.environ['OPENSHIFT_APP_NAME']+'.settings'
+	sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', os.environ['OPENSHIFT_APP_NAME']))
+	application = django.core.handlers.wsgi.WSGIHandler()
+
+else:
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProyectoDjango.settings")
+	application = get_wsgi_application()
+	application = Cling(get_wsgi_application())
 
