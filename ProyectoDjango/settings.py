f1948844101129bbbf9286ec590a409559ddfa4b
hugobarzano <hugobarzano@gmail.com>
2015-11-24 10:15:17 +0100

    configurando despliege con base de datos en opensshift

--- ProyectoDjango/settings.py
+++ ProyectoDjango/settings.py
@@ -90,6 +90,11 @@ if ON_HEROKU:
 	DATABASE_URL='postgres://depwgavwnibefq:Oni7qm6V9p-Ynos0-L0IT8h_zX@ec2-54-83-46-91.compute-1.amazonaws.com:5432/d6f2e1is26nrdm'
 	DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
+ON_OPENSHIFT = os.environ.get('OPENSHIFT_POSTGRESQL_PORT')
+if ON_OPENSHIFT:
+	DATABASE_URL='postgresql://$OPENSHIFT_POSTGRESQL_DB_HOST:$OPENSHIFT_POSTGRESQL_DB_PORT'
+	DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+	
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
