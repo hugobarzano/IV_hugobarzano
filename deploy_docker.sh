3162bf5883f9670eb2dd04295191d4bb7aafae76
hugobarzano <hugobarzano@gmail.com>
2016-02-04 19:08:12 +0100

    docker

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -7,10 +7,3 @@ curl -sSL https://get.docker.com/gpg | sudo apt-key add -
 curl -sSL https://get.docker.com/ | sh
 sudo -i apt-get -y install python-pip
 sudo -i pip install docker-compose
-echo Ejecutando docker-compose
-sudo service docker restart
-sudo docker pull postgres
-sudo docker build -f ~/Ansible/Dockerfile -t aplicacion --no-cache=true .
-echo Voy a esperar 10 segundos a la base de datos
-sleep 10
-sudo docker-compose run web
